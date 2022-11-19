2e8e69aea24e4583516f49877a264316cee16ef7
hw commit <demaniks@yahoo.com>
2022-11-19 16:33:21 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-            //task1
-
+        //task1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
