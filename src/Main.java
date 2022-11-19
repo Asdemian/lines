46a8570950e92773781b7dcaeebab3a2041023be
hw commit <demaniks@yahoo.com>
2022-11-19 16:35:37 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //task1
+        //task2
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
